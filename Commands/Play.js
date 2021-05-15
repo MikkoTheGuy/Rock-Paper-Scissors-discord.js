8c04c3a668868e81e748aef40a7b86538276417e
MikkoTheGuy <84155319+MikkoTheGuy@users.noreply.github.com>
2021-05-15 15:47:34 +0200

    Update Play.js

--- Commands/Play.js
+++ Commands/Play.js
@@ -2,6 +2,35 @@ module.exports = {
 	name: 'play',
 	description: 'start the game',
 	execute(message) {
-		message.channel.send('COMING SOON!');
-	},
+		prefix = "!"
+		const args = message.content.slice(prefix.length).trim().split(/ +/g);
+		const command = args.shift().toLowerCase();
+        const acceptedReplies = ['r', 'p', 's'];
+        const random = Math.floor((Math.random() * acceptedReplies.length));
+        const result = acceptedReplies[random];
+
+        const choice = args[0];
+        if (!choice) return message.channel.send(`How to play: \`${prefix}play <r (rock)|p (paper)|s (scissors)>\``);
+        if (!acceptedReplies.includes(choice)) return message.channel.send(`Only these responses are accepted: \`${acceptedReplies.join(', ')}\``);
+        
+        if (result === choice) return message.reply("It's a tie! We had the same choice.");
+        
+        switch (choice) {
+            case 'r': {
+                if (result === 'paper') return message.reply('I won! i choose paper!');
+                else return message.reply('You won! :(');
+            }
+            case 'p': {
+                if (result === 'scissors') return message.reply('I won! i choose scissors!');
+                else return message.reply('You won! :(');        
+            }
+            case 's': {
+                if (result === 'rock') return message.reply('I won! i choose rock');
+                else return message.reply('You won! :(');
+            }
+            default: {
+                return message.channel.send(`Only these responses are accepted: \`${acceptedReplies.join(', ')}\``);
+            }
+        }
+    },
 };
