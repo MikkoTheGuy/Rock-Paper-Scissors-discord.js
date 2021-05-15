026b46ca85e456ae202592d76a3b33bcc19c4d5e
MikkoTheGuy <84155319+MikkoTheGuy@users.noreply.github.com>
2021-05-15 15:46:58 +0200

    Update Help.js

--- Commands/Help.js
+++ Commands/Help.js
@@ -4,13 +4,13 @@ module.exports = {
 	description: 'Opens the help embed',
 	execute(message) {
 	const exampleEmbed = new Discord.MessageEmbed()
-	.setColor('Black')
+	.setColor('#228B22')
 	.setAuthor('Help!', 'https://i.ibb.co/DK7KY45/icon.png')
 	.setThumbnail('https://cdn.dribbble.com/users/563721/screenshots/3195537/help_02.gif')
 	.addFields(
 		{ name: '!Help', value: 'shows this' },
 		{ name: '!Ping', value: 'ping? pong!' },
-		{ name: '!Play', value: 'COMING SOON' },
+		{ name: '!Play', value: 'Shows how to play the game' },
 	)
 	.setTimestamp()
 	.setFooter('RPS BOT');
